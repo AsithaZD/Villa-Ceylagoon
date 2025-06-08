ac87473450163dc0214a1ba6ad3ef78f93151fb6
asiya99 <akmuthumala@gmail.com>
2025-06-08 21:41:59 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5000,
-  },
+  // server: {
+  //   port: 5000,
+  // },
+  base: "/Villa-Ceylagoon"
 })
